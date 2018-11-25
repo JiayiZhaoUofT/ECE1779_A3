b4433037d4239bb7836af47a8a9190662a24d93b
Yu Hong <Lingyueji@users.noreply.github.com>
2018-11-24 19:44:04 -0500

    add skillMatch

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,8 @@ from flask import Flask
 
 webapp = Flask(__name__)
 
-'''from app import login
-from app import signup
-from app import welcome
-from app import config
-from app import webapp
-from app import imageprocessing'''
-
 from app import config
 from app import upload
+from app import processSkills
+
 
