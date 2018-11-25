6960b91dce349e59f46d21b0d44d53340e8c2a4b
Yu Hong <Lingyueji@users.noreply.github.com>
2018-11-24 20:46:30 -0500

    update

--- app/config.py
+++
@@ -1,8 +0,0 @@
-db_config = {'user': 'root',
-             'password': 'Hy940531',
-             'host': 'localhost',
-             'database': 'ece1779'}
-
-admin_config = {'username': 'admin',
-             'password': 'admin'}
-
