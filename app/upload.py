4e0e775223f664af95fdeb61fd243224efd46aed
JiayiZhaoUofT <37254657+JiayiZhaoUofT@users.noreply.github.com>
2018-11-24 19:41:54 -0500

    upload to s3 fixed

--- app/upload.py
+++ app/upload.py
@@ -1,6 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from app import webapp
 import os
+from app.S3UploadDownload import s3_upload
+
+bucketName = "1779photobucket"
 
 @webapp.route('/',methods=['GET'])
 @webapp.route('/index',methods=['GET'])
@@ -12,7 +15,9 @@ def upload():
 def upload_and_skill_check():
     #todo: upload PDF to s3
     file = request.files['resume']
-    #filepath = os.path.join('app/static/user_images', file.filename)
-    #file.save(filepath)
-    # s3_upload(filepath, filename)
+    filepath = os.path.join('static', file.filename)
+    print(filepath)
+    file.save(filepath)
+    msg = s3_upload(filepath, bucketName,file.filename)
+    print(msg)
     return render_template("/skillCheck.html")
